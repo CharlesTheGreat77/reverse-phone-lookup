d350286596873297dd13689cb3f5557800cd54fd
Charles The Great <27988707+CharlesTheGreat77@users.noreply.github.com>
2023-10-16 09:28:28 -0700

    Update lookup.py

--- lookup.py
+++ lookup.py
@@ -73,24 +73,6 @@ def get_content_from_usphonebook(url):
         html = soup_html(html_content)
         return html
 
-def get_content_from_spydialer(url, phone_number):
-    with sync_playwright() as p:
-        browser = p.webkit.launch()
-        context = browser.new_context()
-        page = context.new_page()
-        stealth_sync(page)
-        page.goto(url)
-        page.fill('input#SearchTextBox', phone_number)
-        page.click('input[value="Search"]')
-        print("[*] Waiting for 6 seconds to click search button..")
-        page.wait_for_timeout(6000)
-        page.click('input[value="Search"]')
-        print("[*] Waiting for 10 seconds to click other search button..")
-        page.wait_for_timeout(10000)
-        html_content = page.content()
-        browser.close()
-        return html_content
-
 def soup_html(html):
     soup = BeautifulSoup(html, 'html.parser')
     html = str(soup.prettify)
@@ -119,30 +101,9 @@ def main():
             html = get_content_from_usphonebook(url)
             target_content = scrape_data(html)
             full_name, addy_list, family_names = get_info(target_content)
-            url = 'https://spydialer.com'
-            target_content = get_content_from_spydialer(url, phone_number)
-            soup = BeautifulSoup(target_content, 'html.parser')
-            name_element = soup.find('a', id='ctl00_ContentPlaceHolder1_NameLinkButton')
-            name = name_element.text if name_element else None
-            message_element = soup.find('span', id='ctl00_ContentPlaceHolder1_DataMessageLabel')
-            message = message_element.text if message_element else None
-            carrier = re.findall(r'\b[A-Z]+(?:\W+[A-Z]+)*', message)
-            for provider in carrier:
-                split_carrier = provider.split(' ')
-                if 'AT&T' in split_carrier:
-                    carrier = 'AT&T'
-                elif 'TMOBILE' in split_carrier:
-                    carrier = 'TMOBILE'
-                elif 'VERIZON' in split_carrier:
-                    carrier = 'Verizon'
-                elif 'METROPCS' in split_carrier:
-                    carrier = 'MetroPCS'
-                else:
-                    carrier = 'Unknown'
 
             print("[*] Results from usphonebook:")
             print(f'Phone Number: {phone_number}\nOwner: {full_name}\nAddress: {addy_list[0]}\nPrior Addresses: {", ".join(addy_list)}\nRelatives & Potentially Past Owners: {", ".join(family_names)}\nAdditional Info: https://usphonebook.com/{phone_number}\n\n')
-            print(f'[*] Results from spydialer\nOwner: {name}\nCarrier: {carrier}\n\n')
 
     if name:
         if name and state:

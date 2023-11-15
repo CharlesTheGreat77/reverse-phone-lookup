e432abd10979a22848c4182fd233c714d287d9ec
Charles The Great <27988707+CharlesTheGreat77@users.noreply.github.com>
2023-11-15 15:23:06 -0800

    Update lookup.py

--- lookup.py
+++ lookup.py
@@ -3,50 +3,84 @@ from bs4 import BeautifulSoup
 import re, random
 from argparse import ArgumentParser
 
-def scrape_data(html):
-    pattern = r'gResults:\s*\'(.*?)\''
-    match = re.search(pattern, html)
-    data = match.group(1)
-    if 'gResults: "null"' in data:
-        data = False
-    else:
-        # tons of cleanup
-        data = re.sub(r'(\w+):', r'"\1":', data)
-        data = data.replace('&quot;', '')
-        data = re.sub(r'(\w+)\s*:\s*true', r'"\1":"true"', data)
-        data = re.sub(r'(\w+)\s*:\s*false', r'"\1":"false"', data)
-        data = data.replace('}', ']').replace("'", '"').replace('{', '[').replace(':', '":"')
-        data = re.sub(r'(\w+)\s*:\s*null', r'"\1":"null"', data)
-        data = '{' + data + '}'
-    return data
-
-def get_info(data):
-    # full name
+def get_url_of_user(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    with open('data.html', 'w') as file:
+        file.write(str(soup.prettify))
     try:
-        pattern = r'fullName":(.*?),'
-        full_name = re.search(pattern, data)
-        full_name = full_name.group(1).replace('"', '')
-    except Exception as msg:
-        print("[-] No data found..")
-        exit(0)
+        link = soup.find_all('input', {'name':'link'})
+        owner = link[-1]['value']
+    except IndexError:
+        a_link = soup.find('a', class_='ls_contacts-btn', href=True)
+        if a_link:
+            owner = a_link['href']
+    url = f'https://usphonebook.com{owner}'
+    return url
 
-    # address and address history
-    pattern = r'fullAddress":(.*?):' 
-    addresses = re.findall(pattern, data)
-    addy_list = []
-    for addy in addresses:
-        addy_list.append(addy.replace(',fullAddressDisplay"', '').replace('"', ''))
+def get_target_information(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    name_span = soup.find('span', class_='header-name')
+    full_name = name_span.get_text(strip=True)
+    address_element = soup.find('p', class_='ls_contacts__text')
+    current_address = address_element.get_text(strip=True)
+    title_tags = soup.find_all('p', class_='ls_contacts__title')
+    associates = []
+    emails = []
+    for title_tag in title_tags:
+        if 'Current Phone Number' in title_tag.get_text(strip=True):
+            person_div = title_tag.find_next('div', itemscope=True)
+            if person_div:
+                phone_number_span = person_div.find('span', itemprop='telephone')
+                # get current phone number
+                current_phone_number = phone_number_span.get_text(strip=True) if phone_number_span else None
+            else:
+                current_phone_number = None
+        elif 'Previous Addresses:' in title_tag.get_text(strip=True):
+            addresses_list = title_tag.find_next('ul')
+            if addresses_list:
+                addresses_list_items = addresses_list.find_all('li')
+                # Extract previous addesses
+                previous_addresses = [item.get_text(strip=True).rsplit(',', 1)[0] for item in addresses_list_items] if addresses_list_items else None
+            else:
+                previous_addresses = None
+        elif 'Previous Phone Numbers:' in title_tag.get_text(strip=True):
+            phone_numbers_list = title_tag.find_next('ul')
+            if phone_numbers_list:
+                phone_numbers_list_items = phone_numbers_list.find_all('li')
+                # Extract individual phone numbers
+                previous_phone_numbers = [item.find('a').get_text(strip=True) if item and item.find('a') else None for item in phone_numbers_list_items]
+            else:
+                previous_phone_numbers = None
+        elif 'Relatives' in title_tag.get_text(strip=True):
+            relatives_section = title_tag.find_next('div', class_='section-relative')
+            if relatives_section:
+                    relatives_items = relatives_section.find_all('span', itemprop='name')
+                    # Extract individual relative names
+                    relatives = [item.get_text(strip=True) for item in relatives_items] if relatives_items else None
+            else:
+                relatives = None
+        elif 'Associates' in title_tag.get_text(strip=True):
+            section_relative_div = title_tag.find_next('div', class_='section-relative')
+            if section_relative_div:
+                associate_links = section_relative_div.find_all('a')
+                associates.extend(span.get_text(strip=True) for span in associate_links)
+            else:
+                associates = None
+        elif 'Email' in title_tag.get_text(strip=True):
+            ul_tag = title_tag.find_next('ul')
+            if ul_tag:
+                email_links = ul_tag.find_all('a', href=lambda x: x and 'mailto' in x)
+                # Extract and append the href attribute of each <a> tag to the emails list
+                emails = [link.get_text(strip=True) for link in email_links] if email_links else None
+            else:
+                emails = None
 
-    # relatives
-    pattern = r'name":(.*?),'
-    relatives = re.findall(pattern, data)
-    family_names = []
-    for name in relatives:
-        if name in relatives:
-            name = name.replace('"', '')
-            family_names.append(name)
-    return full_name, addy_list, family_names
+    associates_list = []
+    if associates != None:
+        for associate in associates:
+            associates_list.append(associate.replace('\n',' '))
 
+    return full_name, current_phone_number, current_address, previous_addresses, previous_phone_numbers, relatives, associates_list, emails
 
 def encode_args(unencoded_args):
     split_list = unencoded_args.split(' ')
@@ -70,16 +104,16 @@ def get_content_from_usphonebook(url):
         context = browser.new_context()
         page = context.new_page()
         page.goto(url)
-        page.wait_for_timeout(random.randint(1500, 2500))
+        page.wait_for_timeout(random.randint(4000, 7500))
         html_content = page.content()
+        url = get_url_of_user(html_content)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto(url)
+        page.wait_for_timeout(random.randint(1500, 2500))
+        html = page.content()
         browser.close()
-        html = soup_html(html_content)
         return html
-
-def soup_html(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    html = str(soup.prettify)
-    return html
 
 
 def main():
@@ -102,11 +136,7 @@ def main():
         else:
             url = f'https://usphonebook.com/{phone_number}'
             html = get_content_from_usphonebook(url)
-            target_content = scrape_data(html)
-            full_name, addy_list, family_names = get_info(target_content)
-
-            print("[*] Results from usphonebook:")
-            print(f'Phone Number: {phone_number}\nOwner: {full_name}\nAddress: {addy_list[0]}\nPrior Addresses: {", ".join(addy_list)}\nRelatives & Potentially Past Owners: {", ".join(family_names)}\nAdditional Info: https://usphonebook.com/{phone_number}\n\n')
+            full_name, current_phone_number, current_address, previous_addresses, previous_phone_numbers, relatives, associates, emails = get_target_information(html)
 
     if name:
         if name and state:
@@ -118,10 +148,20 @@ def main():
             else:
                 url = f'https://usphonebook.com/{encoded_name}/{encoded_state}'
             html = get_content_from_usphonebook(url)
-            target_content = scrape_data(html)
-            full_name, addy_list, family_names = get_info(target_content)
-            print("[*] Results from usphonebook.com")
-            print(f'Phone Number: {phone_number}\nOwner: {full_name}\nAddress: {addy_list[0]}\nPrior Addresses: {", ".join(addy_list)}\nRelatives & Potentially Past Owners: {", ".join(family_names)}\nAdditional Info: {url}\n')
+            full_name, phone_number, current_address, previous_addresses, previous_phone_numbers, relatives, associates, emails = get_target_information(html)
+
+    if 'Current & Past Contact Info' in previous_addresses:
+        previous_addresses = None
+
+    print("[*] Results from usphonebook.com")
+    print(f'Phone Number: {phone_number if phone_number is not None else "N/A"}\n'
+        f'Owner: {full_name if full_name is not None else "N/A"}\n'
+        f'Current Address: {current_address if current_address is not None else "N/A"}\n'
+        f'Prior Addresses: {", ".join(filter(None, previous_addresses)) if previous_addresses else "N/A"}\n'
+        f'Prior Phone Numbers: {", ".join(filter(None, previous_phone_numbers)) if previous_phone_numbers else "N/A"}\n'
+        f'Relatives: {", ".join(filter(None, relatives)) if relatives else "N/A"}\n'
+        f'Associates: {", ".join(filter(None, associates)) if associates else "N/A"}\n'
+        f'Email: {", ".join(filter(None, emails)) if emails else "N/A"}\n\n')
 
 if __name__=='__main__':
     main()
